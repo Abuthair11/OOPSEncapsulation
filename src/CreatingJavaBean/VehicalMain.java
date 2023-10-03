1b2b8c125eec08d3258cec4c6b670f49ca6fad49
Abuthair <Sheikabuthair.pc@gmail.com>
2023-10-03 14:34:49 +0530

    vehical

--- src/CreatingJavaBean/VehicalMain.java
+++ src/CreatingJavaBean/VehicalMain.java
@@ -1,6 +1,23 @@
-package CreatingJavaBean;/**
- * @project OOPS Encapsulation
+package CreatingJavaBean;
+
+/**
  * @author abuthair on 03/10/23.
- *2:09 pm
- */public class VehicalMain {
+ * 2:09 pm
+ * @project OOPS Encapsulation
+ */
+public class VehicalMain {
+
+    public static void main(String[] args) {
+        vehical vehical1 = new vehical();
+        vehical1.setName("BMW");
+        vehical1.setModel("X21");
+        vehical1.setPrice(2111);
+        vehical1.setMillage(34D);
+
+        String ref = vehical1.getName();
+        System.out.println(ref);
+        System.out.println(vehical1.getModel());
+        System.out.println(vehical1.getPrice());
+        System.out.println(vehical1.getMillage());
+    }
 }
--- src/CreatingJavaBean/vehical.java
+++ src/CreatingJavaBean/vehical.java
@@ -1,6 +1,46 @@
-package CreatingJavaBean;/**
- * @project OOPS Encapsulation
+package CreatingJavaBean;
+
+/**
  * @author abuthair on 03/10/23.
- *2:08 pm
- */public class vehical {
+ * 2:08 pm
+ * @project OOPS Encapsulation
+ */
+public class vehical {
+
+    private String name;
+    private String model;
+    private int price;
+    private double millage;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public double getMillage() {
+        return millage;
+    }
+
+    public void setMillage(double millage) {
+        this.millage = millage;
+    }
 }
