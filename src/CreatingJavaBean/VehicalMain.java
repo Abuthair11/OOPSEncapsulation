b804bc863e955abd7049de8b7e1d83c6384b91ec
Abuthair <Sheikabuthair.pc@gmail.com>
2023-10-03 14:31:23 +0530

    Initial commit

---
+++ src/CreatingJavaBean/VehicalMain.java
@@ -0,0 +1,6 @@
+package CreatingJavaBean;/**
+ * @project OOPS Encapsulation
+ * @author abuthair on 03/10/23.
+ *2:09 pm
+ */public class VehicalMain {
+}
---
+++ src/CreatingJavaBean/vehical.java
@@ -0,0 +1,6 @@
+package CreatingJavaBean;/**
+ * @project OOPS Encapsulation
+ * @author abuthair on 03/10/23.
+ *2:08 pm
+ */public class vehical {
+}
